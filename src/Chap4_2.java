0cf9e39180d779d81c9923c14ebda18a083ddab7
Frics <jimmy9711@naver.com>
2020-05-22 01:22:29 +0900

    modify Chap4_2.java

--- src/Chap4_2.java
+++ src/Chap4_2.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
-//chap4_2
+
 class Grade {
     private int subject1, subject2, subject3;
 
-    public Grade(int subject1, int subject2, int subject3){
+    Grade(int subject1, int subject2, int subject3){
         this.subject1 = subject1;
         this.subject2 = subject2;
         this.subject3 = subject3;
     }
-    public int average(){
+    int average(){
         return (subject1+subject2+subject3)/3;
     }
-
 }
 public class Chap4_2{
     public static void main(String[] args){
